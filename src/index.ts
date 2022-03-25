8bc95cc4f88add14465b82286a0a7a3927c830f3
genka <tamani.coding@gmail.com>
2022-03-25 19:34:34 +0100

    init project

---
+++ src/index.ts
@@ -0,0 +1,114 @@
+import { KeyDisplay } from './utils';
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+
+// SCENE
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xa8def0);
+
+// CAMERA
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.y = 5;
+camera.position.z = 5;
+camera.position.x = 0;
+
+// RENDERER
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.shadowMap.enabled = true
+
+// CONTROLS
+const orbitControls = new OrbitControls(camera, renderer.domElement);
+orbitControls.enableDamping = true
+orbitControls.minDistance = 5
+orbitControls.maxDistance = 15
+orbitControls.enablePan = false
+orbitControls.maxPolarAngle = Math.PI / 2 - 0.05
+orbitControls.update();
+
+// LIGHTS
+light()
+
+// FLOOR
+generateFloor()
+
+// CONTROL KEYS
+const keysPressed = {  }
+const keyDisplayQueue = new KeyDisplay();
+document.addEventListener('keydown', (event) => {
+    keyDisplayQueue.down(event.key)
+}, false);
+document.addEventListener('keyup', (event) => {
+    keyDisplayQueue.up(event.key);
+    (keysPressed as any)[event.key.toLowerCase()] = false
+}, false);
+
+const clock = new THREE.Clock();
+// ANIMATE
+function animate() {
+    let mixerUpdateDelta = clock.getDelta();
+    orbitControls.update()
+    renderer.render(scene, camera);
+    requestAnimationFrame(animate);
+}
+document.body.appendChild(renderer.domElement);
+animate();
+
+// RESIZE HANDLER
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    keyDisplayQueue.updatePosition()
+}
+window.addEventListener('resize', onWindowResize);
+
+function generateFloor() {
+    // TEXTURES
+    const textureLoader = new THREE.TextureLoader();
+    const placeholder = textureLoader.load("./textures/placeholder/placeholder.png");
+
+    const WIDTH = 4
+    const LENGTH = 4
+    const NUM_X = 15
+    const NUM_Z = 15
+
+    const geometry = new THREE.PlaneGeometry(WIDTH, LENGTH, 512, 512);
+    const material = new THREE.MeshStandardMaterial(
+        {
+            map: placeholder
+        })
+    // const material = new THREE.MeshPhongMaterial({ map: placeholder})
+
+    for (let i = 0; i < NUM_X; i++) {
+        for (let j = 0; j < NUM_Z; j++) {
+            const floor = new THREE.Mesh(geometry, material)
+            floor.receiveShadow = true
+            floor.rotation.x = - Math.PI / 2
+
+            floor.position.x = i * WIDTH - (NUM_X / 2) * WIDTH
+            floor.position.z = j * LENGTH - (NUM_Z / 2) * LENGTH
+
+            scene.add(floor)
+        }
+    }
+}
+
+function light() {
+    scene.add(new THREE.AmbientLight(0xffffff, 0.7))
+
+    const dirLight = new THREE.DirectionalLight(0xffffff, 1)
+    dirLight.position.set(- 60, 100, - 10);
+    dirLight.castShadow = true;
+    dirLight.shadow.camera.top = 50;
+    dirLight.shadow.camera.bottom = - 50;
+    dirLight.shadow.camera.left = - 50;
+    dirLight.shadow.camera.right = 50;
+    dirLight.shadow.camera.near = 0.1;
+    dirLight.shadow.camera.far = 200;
+    dirLight.shadow.mapSize.width = 4096;
+    dirLight.shadow.mapSize.height = 4096;
+    scene.add(dirLight);
+    // scene.add( new THREE.CameraHelper(dirLight.shadow.camera))
+}
